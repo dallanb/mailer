213c99beceb1829a08dded81fc3ceb61089f4a9d
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-07-21 15:01:41 -0700

    continue following tutorial and begin looking into creating api

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # mailer
+
 MAILER Microservice
 
 ## Background
+
 This service provides mailing for the application. The framework used for this service is Slim.
 
 ## Slim Setup
+
 http://www.slimframework.com/docs/v3/tutorial/first-app.html
--- src/public/index.php
+++ src/public/index.php
@@ -1,15 +1,35 @@
 <?php
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
-$app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
+$config['displayErrorDetails'] = true; # display errors in dev
+$config['addContentLengthHeader'] = false;
 
-    return $response;
+$app = new \Slim\App(['settings' => $config]);
+
+# dependency injection container used to load dependencies for the app
+$container = $app->getContainer();
+
+# configure logging
+$container['logger'] = function ($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler('../logs/tapir.log');
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+$app->get('/ping', function (
+    Request $request,
+    Response $response,
+    array $args
+) {
+    $this->logger->addInfo('ping');
+    $data = ['name' => 'Rob', 'age' => 40];
+    return $response->withJson(['msg' => 'OK', 'data' => ['message' => 'pong']],
+        200
+    );
 });
 $app->run();
