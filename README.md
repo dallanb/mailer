0549ee2513f7bc40a9958a9f7f98ba1392e8f6cf
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-07-21 12:12:34 -0700

    set up initial with slim

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mailer
 MAILER Microservice
+
+## Background
+This service provides mailing for the application. The framework used for this service is Slim.
+
+## Slim Setup
+http://www.slimframework.com/docs/v3/tutorial/first-app.html
--- src/public/index.php
+++ src/public/index.php
@@ -1,45 +1,15 @@
 <?php
 
-// Valid PHP Version?
-$minPHPVersion = '7.2';
-if (phpversion() < $minPHPVersion)
-{
-	die("Your PHP version must be {$minPHPVersion} or higher to run CodeIgniter. Current version: " . phpversion());
-}
-unset($minPHPVersion);
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-// Path to the front controller (this file)
-define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+require '../vendor/autoload.php';
 
-// Location of the Paths config file.
-// This is the line that might need to be changed, depending on your folder structure.
-$pathsPath = realpath(FCPATH . '../app/Config/Paths.php');
-// ^^^ Change this if you move your application folder
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
 
-/*
- *---------------------------------------------------------------
- * BOOTSTRAP THE APPLICATION
- *---------------------------------------------------------------
- * This process sets up the path constants, loads and registers
- * our autoloader, along with Composer's, loads our constants
- * and fires up an environment-specific bootstrapping.
- */
-
-// Ensure the current directory is pointing to the front controller's directory
-chdir(__DIR__);
-
-// Load our paths config file
-require $pathsPath;
-$paths = new Config\Paths();
-
-// Location of the framework bootstrap file.
-$app = require rtrim($paths->systemDirectory, '/ ') . '/bootstrap.php';
-
-/*
- *---------------------------------------------------------------
- * LAUNCH THE APPLICATION
- *---------------------------------------------------------------
- * Now that everything is setup, it's time to actually fire
- * up the engines and make this app do its thang.
- */
+    return $response;
+});
 $app->run();
